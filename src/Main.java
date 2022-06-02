28f85e00200497c81b1630f2850372894b7440dc
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-06-01 17:45:32 -0700

    Created framework and detailed challenge

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+Write a method with the name sumDigits that has one int parameter called number.
+If the parameter is >=10 then the method should proces the number and return sum of all digits, otherwise return -1 to indicate invalid value.
+The numbers from 00-9 have 1 digit so we dont want to process them, also we dont want to process negative numbers so we also return -1 for negative numbers.
+For example calling the method sumDigits(125) should return 8 since 1+2+5=8.
+Calling the method sumDigits(1) should return -1 as per requirements described above.
+Add some code to the main method to test out the sumdigits method to determine that it is working correctly for valid and invalid values passed as arguments.
+
+Hint use n%10 to extract the least significant digit.
+use n=n/10 to discard the least significant digit.
+the method needs to be static like other methods so far in the course.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
