aed030c9474bc036a58c9706791a227bb71f44a7
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-06-01 17:57:14 -0700

    Challenge solved!

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,22 @@ the method needs to be static like other methods so far in the course.
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(sumDigits(125));
+    }
+    public static int sumDigits(int number){
+        int sum = 0;
 
+        if (number >= 10){
+            //return sum of all digits here
+            while (number / 10 > 0){
+                sum += (number % 10);
+                number = number / 10;
+            }
+            sum += number;
+            return sum;
+        }
+        else{
+            return -1;
+        }
     }
 }
